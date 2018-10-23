94ed6271426ce9e04747da81ff82e097873fdad7
Барышев Владимир Александрович <Baryshev.V.A@sberbank.ru>
2018-10-23 12:42:09 +0300

    HW03 in Linal added

--- hse/Linear_algebra/HW3/hw_3.py
+++ hse/Linear_algebra/HW3/hw_3.py
@@ -4,18 +4,18 @@ from numpy.linalg import matrix_power
 
 # Управжнение №1
 def count(x):
-	return x**3-6*x**2+11*x-6
+	return x**3-4*x**2+5*x-2
 
-for i in range(-10, 10):
+for i in range(-20, 20):
 	r = count(i) 
 	print(i, r)
 
-# Проверка решения
-T = [[1,1,1],[1,0,2],[2,1,2]]
-B = [[1,0,0],[0,2,0],[0,0,3]]
-TT = [[-2,-1,2],[2,0,-1],[1,1,-1]] #T-1
+# # Проверка решения
+# T = [[1,1,1],[1,0,2],[2,1,2]]
+# B = [[1,0,0],[0,2,0],[0,0,3]]
+# TT = [[-2,-1,2],[2,0,-1],[1,1,-1]] #T-1
 
-X = np.matmul(np.matmul(T,B), TT)
-print(X)
+# X = np.matmul(np.matmul(T,B), TT)
+# print(X)
 
 
